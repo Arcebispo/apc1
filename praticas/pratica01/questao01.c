de0cfffe2a8fb492ee6f5d9b0bec71b8174c4665
Arcebispo <142992313+Arcebispo@users.noreply.github.com>
2023-09-20 13:55:52 +0000

    20/09/2023

--- praticas/pratica01/questao01.c
+++ praticas/pratica01/questao01.c
@@ -5,11 +5,29 @@ calcule a media final a partir da formula (0,4 x A1) + (0.6 x A2).Considere A1 e
 #include <stdio.h>
 int main()
 {
-float nota01 = 5.5f;
-float nota02 = 6.0f;
+//float nota01 = 5.5f;
+//float nota02 = 6.0f;
+float nota01;
+float nota02;
 
+printf("Media temque ser superior a 5\n");  
+printf("Informe a primeira nota:");
+scanf("%f",&nota01);
+printf("Informe a segunda nota:");
+scanf("%f",&nota02);
 float media = 0.4 * nota01 + 0.6 * nota02;
 printf("A média é %f\n",media);
-
+if (media > 5)
+{
+printf("APROVAÇÃO ACIMA DA MÉDIA");
+}
+else if(media == 5)
+{
+printf("APROVAÇÃO NA MÉDIA");
+} 
+else
+{
+printf("REPROVAÇÃO abaixo DA MÉDIA");       
+}
 return 0;
 }
--- praticas/pratica01/questao05.c
+++ praticas/pratica01/questao05.c
@@ -1,16 +1,19 @@
 #include <stdio.h>
+#include <math.h>
 
 int main (){
 
   const float giga = 1024;
   const float quantidade = 5;
-
-
-
   
   float total_bite = quantidade*giga;
 
-  printf("Quantidade de bites é %f\n", total_bite);
+  printf("--------Quantidade de bites é %f--------\n\n", total_bite);
 
+  double valorEMgiga = 10;
+  double valorEMbytes = valorEMgiga * pow(1024,3);
+  
+  printf("--------Valor em giga %f em bytes é --------%f\n\n", valorEMgiga , valorEMbytes);
+  
   return 0;
 }
